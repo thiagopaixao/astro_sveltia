63b6e6f971b74b913018b4aa561e2e726ab0c31c
Bruno <bruno.berken@gmail.com>
2024-11-12 17:26:14 -0300

    [del] remove setColorsVars js util

--- src/assets/scripts/utils/setColorsVars.js
+++
@@ -1,7 +0,0 @@
-export function SetColorsVars(colors = {}) {
-  if(typeof colors !== 'object') return
-
-  return Object.entries(colors).reduce((acc, curr) => {
-    return curr[1] ? acc + `--clr-${curr[0]}: ${curr[1]};` : acc;
-  }, '');
-}
