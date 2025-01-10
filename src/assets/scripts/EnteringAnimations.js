87d3e317adf29a6cc9746f54632bcaf58aedb430
Bruno <bruno.berken@gmail.com>
2025-01-10 17:29:40 -0300

    [add] WIP adding reveal animations to modules

---
+++ src/assets/scripts/EnteringAnimations.js
@@ -0,0 +1,30 @@
+import ScrollObserver from './utils/ScrollObserver';
+
+export default class EnteringAnimations {
+  constructor() {
+    this.elementsToReveal = document.querySelectorAll('[class*="reveal--"]');
+
+    if(!this.elementsToReveal.length) return;
+
+    this.revealElements();
+  }
+
+  revealElements() {      
+    this.elementsToReveal.forEach((item, index) => {
+      const scrollObserver = new ScrollObserver();
+  
+      scrollObserver.observe(item, (data) => {
+        const { element, isVisible, boundingRect } = data;
+                
+        if(!isVisible || item.classList.contains('reveal')) return;
+  
+        const windowStartingPoint = window.innerHeight * .85;      
+        const isInRange = boundingRect.top <= windowStartingPoint && boundingRect.bottom >= 0;
+  
+        if (isInRange) {
+          element.classList.add('reveal');
+        }
+      });
+    });
+  }
+}
